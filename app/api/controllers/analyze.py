ec9950c62e82dd954f6450eb0bf899e8f6a35381
Wander Vinicius Bergami <wander.bergami@gmail.com>
2025-06-18 13:30:21 -0300

    Use EmailStr for email validation

--- app/api/controllers/analyze.py
+++ app/api/controllers/analyze.py
@@ -1,18 +1,17 @@
-from fastapi import APIRouter, UploadFile, File, Query, HTTPException
+from fastapi import APIRouter, UploadFile, File, Query
+from pydantic import EmailStr
 from app.services.analyze_service import AnalyzeService
 
 router = APIRouter()
 
 @router.post("/analyze_document")
 async def analyze_document(
-    email: str = Query(..., description="E-mail do usuário para identificação"),
+    email: EmailStr = Query(..., description="E-mail do usuário para identificação"),
     file: UploadFile = File(...)
 ):
     """Recebe um PDF, valida o e-mail e retorna os metadados extraídos."""
 
-    # # 🔍 **Validação do E-mail**
-    # if not is_valid_email(email):
-    #     raise HTTPException(status_code=400, detail="E-mail inválido")
+
 
     # 🛠 **Processamento do Documento**
     extracted_data = await AnalyzeService.process_document(file, email)
